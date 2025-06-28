c92a3dbda3fe33e355e521d73c105c85cc1bf38b
tugas45 <racerftail@gmail.com>
2025-06-29 00:48:54 +0700

    test

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:resepku/home.dart';
+import 'package:resepku/splashscreen.dart';
+import 'package:resepku/favorit.dart';
+import 'package:resepku/detail.dart';
+import 'package:resepku/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ResepKu',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
