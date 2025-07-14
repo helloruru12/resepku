d2f721b1d3cc5174be0bd090dc2fde3659899985
Amara <amararahmaning@gmail.com>
2025-07-14 10:53:45 +0700

    tambah url dan anonkey

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:resepku/login.dart';
 import 'package:resepku/splashscreen.dart';
-import 'package:resepku/home.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://silrdympepkqpiuxvadh.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpbHJkeW1wZXBrcXBpdXh2YWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MjY0NjksImV4cCI6MjA2NzUwMjQ2OX0.ARMa6jG-WQyUfzxE0SPLhtFG6sVc8FBMPCCCsmXEZ64',
+  );
   runApp(const MyApp());
 }
 
@@ -11,13 +17,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: HomePage(),
+      home: SplashScreen(),
     );
   }
 }
